1ca4108c933bf7d53bf9c7942f9dde0d7342b40c
Bakr Hawsawi <bakrhawsawi4@gmail.com>
2023-10-14 00:00:10 +0300

    start appComponets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'serverapp';
+  constructor(private serverService: ServerService) {
+
+  }
 }
